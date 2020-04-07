31e73d22f541b812168909e62e9a9ae5108020f4
Kris Seraphine <kris.seraphine@cdw.com>
2020-04-07 00:15:11 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import io
+import re
+from setuptools import setup, find_packages
+
+_version_rgx = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with io.open("./click_keyring/__init__.py", "rt", encoding="utf8") as fh:
+    version = _version_rgx.search(fh.read()).group(1)
+
+setup(
+    name='click-keyring',
+    version=version,
+    description='Save and retrieve passwords using Keyring with Click',
+    author='Kris Seraphine',
+    author_email='kris.seraphine@cdw.com',
+    url='https://github.com/cdwlabs/click-keyring',
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'cryptography',
+        'keyring',
+    ],
+)
